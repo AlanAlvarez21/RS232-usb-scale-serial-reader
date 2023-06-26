ff44d4753cf651c8c71d507b718b3dd32c62591e
Alan Daniel Alvarez Castro <alandanielalvarez0000@gmail.com>
2023-06-26 09:56:54 -0600

    feat: updated excel uploader script

--- excel_uploader.py
+++ excel_uploader.py
@@ -8,17 +8,12 @@ scope = [
     "https://www.googleapis.com/auth/drive.readonly",
     "https://www.googleapis.com/auth/drive.metadata.readonly",
     "https://www.googleapis.com/auth/spreadsheets"
-]
+        ]
 
 credentials = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
 client = gspread.authorize(credentials)
-
 sheet = client.open("tutorial").sheet1
 
-data = sheet.get_all_records()
 
-# row = sheet.row_values(1)
-# col = sheet.column_values(1)
-cell = sheet.cell(1,1).value
-sheet.update_cell(1,1, ')*(')
+sheet.update_cell(2,1, 'peso')
 
